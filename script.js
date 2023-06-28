acc7cfa10d43a3acb1fdab228c59eea64303597c
Santiago <santi11886@gmail.com>
2023-06-27 20:42:03 -0500

    .

--- script.js
+++ script.js
@@ -4,11 +4,3 @@ document.querySelector("button.button-menu-toggle")
 .addEventListener("click", function() {
     document.querySelector(".nav-links").
     classList.toggle("nav-links-responsive")})
-
-    function anterior() {
-        console.log("mostrar slide anterior")
-    }
-
-    function siguiente() {
-        console.log("mostrar slide siguiente")
-    }
