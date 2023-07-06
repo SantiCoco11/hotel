0f4882dbb37deb2b81a7fc686627088d92174bab
Santiago <santi11886@gmail.com>
2023-07-05 21:26:03 -0500

    modificacion de fotos

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ document.querySelector("button.button-menu-toggle")
 .addEventListener("click", function() {
     document.querySelector(".nav-links").
     classList.toggle("nav-links-responsive")})
+
+    // JavaScript para controlar el slider
+const slider = document.querySelector('.slider');
+const menuSlider = document.querySelectorAll('.menu-slider a');
+
+function showSlide(slideIndex) {
+    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+    menuSlider.forEach((menuItem, index) => {
+        if (index === slideIndex) {
+            menuItem.classList.add('active');
+        } else {
+            menuItem.classList.remove('active');
+        }
+    });
+}
+
+// Mostrar el primer slide al cargar la página
+showSlide(0);
